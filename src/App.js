53b4c88de970dba44c520b8e5f618b8fba5a39e6
Your Name <jennydinh5@gmail.com>
2023-03-24 09:20:27 -0400

    TEST

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Jennifer and MSU CAS!
+          Learn React with Jennifer and MSU CAS TEST!
         </a>
       </header>
     </div>
