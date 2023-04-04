9714aaefa9b2853db316369bdf0e3097b53481e7
Your Name <jennydinh5@gmail.com>
2023-04-03 22:48:17 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
-// import logo from './logo.svg';
+import { useState } from 'react';
+import { supabase } from './supabaseClient';
+import logo from './logo.svg';
 import './App.css';
 
+function Library() {
+  const [myBooks, setMyBooks] = useState([0]);
+  async function getBooks() {
+    let { data: books, error } = await supabase
+      .from('books')
+      .select('*')
+    setMyBooks(books);
+  }
+  
+  getBooks();
+  return (
+  <>
+    <table>
+    {
+      myBooks.map(b => (
+        <tr>
+          className = "tableBooks"
+          <td>{b.title}</td>
+          <td>{b.author}</td>
+          <td>{b.isbn}</td>
+        </tr>
+      ))
+    }
+    </table>
+      <button onClick = {getBooks}>books</button>
+  </>
+  );
+}
 
 const album = [
   { id: 1, title: 'All 4 Nothing', theme: 'plain', isRecent: true },
@@ -29,7 +59,7 @@ const song = {
   songtitle: 'Modern Loneliness',
   artist: 'Lauv',
   released: '2019',
-  image: 'https://www.thekirkwoodcall.com/wp-content/uploads/2020/03/Project-Drawing-1_98-e1583334295912.png',
+  image: logo,
   width: '220',
   height: '330'
 };
@@ -55,10 +85,14 @@ function Songarea() {
 }
 
 function MusicButton() {
+  const [count, setCount] = useState(0)
+  function doMagic () {
+    setCount(count + 1);
+  }
   return (
     <div>
-      <h3>Click here to play music!</h3>
-      <button>Music</button>
+      <h3>This is a magic button</h3>
+      <button onClick = {doMagic}>Magic {count}</button>
     </div>
   );
 }
@@ -67,6 +101,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Library />
         <AlbTrack />
         <Songarea />
         <MusicButton />
